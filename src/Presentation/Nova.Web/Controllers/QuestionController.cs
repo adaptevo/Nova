48e1f8baaf9d28b326bb0ad6fad1d971b5ff6fd1
josephajose <joseph.a.jose@gmail.com>
2013-07-15 22:16:35 -0700

    Added Question Controller

---
+++ src/Presentation/Nova.Web/Controllers/QuestionController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nova.Web.Controllers
+{
+    public class QuestionController : Controller
+    {
+        //
+        // GET: /Question/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
