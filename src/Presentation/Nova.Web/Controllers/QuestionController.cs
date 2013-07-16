8dbf892416e4167b19e763396e14263e5468434e
josephajose <joseph.a.jose@gmail.com>
2013-07-15 22:53:43 -0700

    Added in logic to save new question in temporary repository in controller

--- src/Presentation/Nova.Web/Controllers/QuestionController.cs
+++ src/Presentation/Nova.Web/Controllers/QuestionController.cs
@@ -8,10 +8,12 @@ namespace Nova.Web.Controllers
 {
     public class QuestionController : Controller
     {
+        private static readonly Dictionary<string, string> _questionRepository = new Dictionary<string, string>();
+
         //
         // GET: /Question/
 
-        public ActionResult Index()
+        public ActionResult Index(string QuestionId)
         {
             return View();
         }
@@ -21,9 +23,11 @@ namespace Nova.Web.Controllers
             return View("CreateQuestionView");
         }
 
-        public ActionResult SaveNew(string Question)
+        public ActionResult SaveNew(string questionText)
         {
-            return View();
+            string questionId = Guid.NewGuid().ToString();
+            _questionRepository.Add(questionId, questionText);
+            return Index(questionId);
         }
     }
 }
