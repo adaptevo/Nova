96ca18bea87594884b360de815480b8173d4be79
josephajose <joseph.a.jose@gmail.com>
2013-07-15 23:50:43 -0700

    Added Question model with a CheckArgument utitlity

---
+++ src/Libraries/Nova.Core/Model/Question.cs
@@ -0,0 +1,15 @@
+﻿using Nova.Core.Utilities;
+
+namespace Nova.Core.Model
+{
+    public class Question
+    {
+        public Question(string questionText)
+        {
+            QuestionText = CheckArgument.NotNull(questionText, "questionText");
+        }
+
+        public string QuestionId { get; private set; }
+        public string QuestionText { get; set; }
+    }
+}
---
+++ src/Libraries/Nova.Core/Utilities/CheckArgument.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Nova.Core.Utilities
+{
+    public static class CheckArgument
+    {
+        public static T NotNull<T>(T value, string argument) where T : class
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(argument);
+            }
+
+            return value;
+        }
+    }
+}
