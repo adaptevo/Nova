35aa0f86797bee1134ff5a835f97807802018b18
scui <seancui717@gmail.com>
2013-07-25 22:36:26 -0700

    adding Ioc implementation using Windsor
    not finished yet. Want to check in so that I can continue working on it
    tmr in the office.

---
+++ src/Libraries/Nova.Ioc/IocContainer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Nova.Libraries.Ioc
+{
+    public static class IocContainer
+    {
+        public static void RegisterTypesForApplication(IList<string> assemblyPaths)
+        {
+            IList<string> assembliesToRegister = new List<string>();
+
+            foreach (string assemblyPath in assemblyPaths)
+            {
+                try
+                {
+                    RegisterTypesInAssembly(Assembly.LoadFrom(assemblyPath));
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        private static void RegisterTypesInAssembly(Assembly assembly)
+        {
+            IList<Type> types = new List<Type>(assembly.GetTypes());
+
+            foreach (Type type in types)
+            {
+                if (typeof(IocModule).IsAssignableFrom(type) && !type.IsAbstract)
+                {
+                    IocModule iocModule = (IocModule)Activator.CreateInstance(type);
+                    //iocModule.RegisterTypes();
+                }
+            }
+        }
+    }
+}
