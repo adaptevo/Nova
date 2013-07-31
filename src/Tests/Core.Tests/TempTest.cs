9a7275dbb3bc05b727c710fea2a1f978145da190
josephajose <joseph.a.jose@gmail.com>
2013-07-30 23:57:10 -0700

    Added test project for Nova.Core as Nova.Core.Tests.  It uses xUnit testing framework with moq as the mocking framework and autofixture for automating fixture setup

---
+++ src/Tests/Core.Tests/TempTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Nova.Core.Tests
+{
+    public class TempTest
+    {
+        [Fact]
+        public void SomeMethod_GivenSomeArguments_ReturnsSomeResult()
+        {
+            Assert.Equal(3.0, 1.0 + 2.0); 
+        }
+    }
+}
